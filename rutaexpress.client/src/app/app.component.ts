708c924bcaa7ec13e7b21a6953c25ce35cd6b8f2
fvallejosdev <fvallejos.dev@gmail.com>
2024-06-26 16:57:13 -0500

    add flowbite

--- rutaexpress.client/src/app/app.component.ts
+++ rutaexpress.client/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'WebApp';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
+  title = 'RutaExpress';
 }
